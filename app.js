75686002c21ac147fb152bfdbc18d65d2f15bc28
8wallDawn <dankthedust@gmail.com>
2021-11-06 11:35:12 +0900

    dom api를 이용한 방법에서  문자열을 이용한 html 요소 생성하기

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const container = document.getElementById('root')
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false) // 데이터를 동기적으로 가져오겠다.
-ajax.send(); // 데이터를 가져온다.
+function getData(url) {
+    ajax.open('GET', url, false) // 데이터를 동기적으로 가져오겠다.
+    ajax.send(); // 데이터를 가져온다.
 
-const newsFeed = JSON.parse(ajax.response);
+    return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 window.addEventListener('hashchange', function() {
     // console.log('해시가 변경되었습니다.')
     // console.log(this.location) url 주소에 대한 정보로 hash에 대한 값을 추출 가능.
     const id = this.location.hash.substr(1) // #으로 시작하는 해쉬값에서 #을 제거하여 id 값만을 순수하게 추출하여 저장
 
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id))
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title
@@ -35,13 +36,10 @@ window.addEventListener('hashchange', function() {
 // </ul>`;
 
 newsFeed.forEach(feed => {
-    const li = document.createElement('li')
-    const a = document.createElement('a')
-    a.href=`#${feed.id}`;
-    ul.appendChild(li);
-    li.appendChild(a);
-    a.innerHTML = `${feed.title} (${feed.comments_count})`;
+    const div = document.createElement('div'); // 임시적인 div 태그 생성
     
+    div.innerHTML = `<li><a href='#${feed.id}'>${feed.title} (${feed.comments_count})</a></li>`
+    ul.appendChild(div.firstElementChild); // div태그를 제외하고 내부에 출력할 li 태그 만을 ul 내에 자식요소로 추가
 });
 
 container.appendChild(ul);
