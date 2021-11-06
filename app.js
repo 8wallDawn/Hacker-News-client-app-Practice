80aca79c30c96549174c47d24aa2d63590d2d94b
8wallDawn <dankthedust@gmail.com>
2021-11-06 15:24:25 +0900

    페이징

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
+const store = {
+	currentPage: 1
+}
 function getData(url) {
 	ajax.open('GET', url, false) // 데이터를 동기적으로 가져오겠다.
 	ajax.send(); // 데이터를 가져온다.
@@ -14,14 +16,27 @@ function getData(url) {
 function newsFeed() {
 	const newsFeed = getData(NEWS_URL);
 	const newsList = [];
+	const maxPage = newsFeed%10 === 0 ? newsFeed/10 : newsFeed+1;
 
 	newsList.push('<ul>')
 
-	newsFeed.forEach(feed => {
-		newsList.push(`<li><a href='#${feed.id}'>${feed.title} (${feed.comments_count})</a></li>`)
-	});
+	for(let i = (store.currentPage -1) * 10; i < store.currentPage*10; ++i) {
+		newsList.push(`
+			<li>
+				<a href='#/show/${newsFeed[i].id}'>
+					${newsFeed[i].title} (${newsFeed[i].comments_count})
+				</a>
+			</li>
+		`)
+	};
 
 	newsList.push('</ul>')
+	newsList.push(`
+		<div>
+			<a href='#/page/${store.currentPage > 1 ? store.currentPage-1 : 1}'>이전 페이지</a>
+			<a href='#/page/${store.currentPage === maxPage ? maxPage : store.currentPage+1}'>다음 페이지</a>
+		</div>
+	`)
 	// console.log(newsList);
 	container.innerHTML = newsList.join('');
 }
@@ -29,10 +44,10 @@ function newsFeed() {
 function newsDetail() {
 	// console.log('해시가 변경되었습니다.')
 	// console.log(this.location) url 주소에 대한 정보로 hash에 대한 값을 추출 가능.
-	const id = location.hash.substr(1) // #으로 시작하는 해쉬값에서 #을 제거하여 id 값만을 순수하게 추출하여 저장
+	const id = location.hash.substr(7) // #으로 시작하는 해쉬값에서 #을 제거하여 id 값만을 순수하게 추출하여 저장
 	const newsContent = getData(CONTENT_URL.replace('@id', id));
 
-	container.innerHTML = `<h1>${newsContent.title}</h1><div><a href='#'>목록으로</div>`;
+	container.innerHTML = `<h1>${newsContent.title}</h1><div><a href='#/page/${store.currentPage}'>목록으로</div>`;
 }
 
 
@@ -40,8 +55,12 @@ function newsDetail() {
 // 화면 전환의 주요 쟁점
 function router() {
 	const routePath = location.hash;
+	// console.log(routePath); // #/page/(number)
 
 	if (routePath === '') {
+		newsFeed();
+	} else if(routePath.indexOf('#/page/') >= 0){
+		store.currentPage = +routePath.substr(7);
 		newsFeed();
 	} else {
 		newsDetail();
