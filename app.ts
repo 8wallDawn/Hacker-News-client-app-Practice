a13583f4eafa92f6b8b31d088248a4a5ac1829a3
8wallDawn <dankthedust@gmail.com>
2021-11-17 22:36:52 +0900

    View 클래스 추가를 통한 코드 구조 개선과 private와 protected를 통한 접근 제어

--- app.ts
+++ app.ts
@@ -13,9 +13,9 @@ interface News {
 }
 
 interface NewsFeed extends News {
-	readonly comments_count : number;
+	readonly comments_count: number;
 	readonly points: number;
-	read?: boolean;
+	read ? : boolean;
 }
 
 interface NewsDetail extends News {
@@ -25,6 +25,11 @@ interface NewsDetail extends News {
 interface NewsComment extends News {
 	readonly comments: NewsComment[];
 	level: number;
+}
+
+interface RouteInfo {
+	path: string;
+	page: View;
 }
 
 const container: HTMLElement | null = document.getElementById('root')
@@ -37,12 +42,12 @@ const store: Store = {
 	feeds: [],
 }
 
-function applyApiMixins(targetClass:any, baseClasses:any[]): void {
+function applyApiMixins(targetClass: any, baseClasses: any[]): void {
 	baseClasses.forEach(baseClass => {
 		Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
 			const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
 
-			if(descriptor) {
+			if (descriptor) {
 				Object.defineProperty(targetClass.prototype, name, descriptor);
 			}
 		});
@@ -50,24 +55,24 @@ function applyApiMixins(targetClass:any, baseClasses:any[]): void {
 }
 
 class Api {
-  getRequest<AjaxResponse>(url:string): AjaxResponse {
+	getRequest < AjaxResponse > (url: string): AjaxResponse {
 		const ajax = new XMLHttpRequest();
 		ajax.open('GET', url, false) // 데이터를 동기적으로 가져오겠다.
 		ajax.send(); // 데이터를 가져온다.
-	
+
 		return JSON.parse(ajax.response);
 	}
 }
 
 class NewsFeedApi {
 	getData(): NewsFeed[] {
-		return this.getRequest<NewsFeed[]>(NEWS_URL);
+		return this.getRequest < NewsFeed[] > (NEWS_URL);
 	}
 }
 
 class NewsDetailApi {
-	getData(id:string): NewsDetail {
-		return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id',id));
+	getData(id: string): NewsDetail {
+		return this.getRequest < NewsDetail > (CONTENT_URL.replace('@id', id));
 	}
 }
 
@@ -76,28 +81,96 @@ interface NewsDetailApi extends Api {};
 applyApiMixins(NewsFeedApi, [Api]);
 applyApiMixins(NewsDetailApi, [Api]);
 
-function makeFeeds(feeds:NewsFeed[]): NewsFeed[] {
-	for( let i=0; i < feeds.length; ++i) {
-		feeds[i].read = false;
+abstract class View {
+	private template : string;
+	private renderTemplate: string;
+	private container : HTMLElement;
+	private htmlList :string[];
+
+	constructor(containerId:string, template:string) {
+		const containerElement = document.getElementById(containerId);
+
+		if(!containerElement) {
+			throw '최상위 컨테이너가 없어 UI를 진행하지 못합니다.';
+		}
+
+		this.container = containerElement;
+		this.template = template;
+		this.renderTemplate = template;
+		this.htmlList = [];
 	}
-	return feeds;
+
+	protected updateView(): void {
+		this.container.innerHTML = this.renderTemplate;
+		this.renderTemplate = this.template;
+	}
+
+	protected  addHtml(htmlString:string): void {
+		this.htmlList.push(htmlString);
+	}
+
+	protected getHtml(): string {
+		const snapshot= this.htmlList.join('');
+		this.clearHtmlList();
+		return snapshot;
+	}
+
+	protected setTemplateData(key: string, value: string): void{
+		this.renderTemplate = this.renderTemplate.replace(`{{__${key}__}}`, value)
+	}
+
+	private clearHtmlList(): void{
+		this.htmlList = [];
+	}
+
+	abstract render():void; // 상속받는 class에 해당하는 자식 메소드를 구현하라는 의미의 마킹으로 '추상 메소드' 이다.
 }
 
-function updateView(html:string): void {
-	if(container != null){
-		container.innerHTML = html;
-	} else {
-		console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.')
+// 해쉬값에 따라 어떤 함수를 실행하여 사용자에게 제공할 것인지리를 판단하는 함수.
+// 화면 전환의 주요 쟁점
+class Router {
+	routeTable: RouteInfo[];
+	defaultRoute: RouteInfo | null;
+
+	constructor() {
+		// console.log(routePath); // #/page/(number)
+		window.addEventListener('hashchange', this.route.bind(this));
+	
+		this.routeTable = [];
+		this.defaultRoute = null;
 	}
+
+	setDefaultPage(page:View): void {
+		this.defaultRoute = {path:'', page}
+	}
+
+	addRoutePath(path: string, page: View): void {
+		this.routeTable.push({path, page});
+	}
+
+	route():void {
+		const routePath = location.hash;
+
+		if(routePath === '' && this.defaultRoute) {
+			this.defaultRoute.page.render(); // abstract를 통한 render() 의 사용
+		}
+
+		for (const routeInfo of this.routeTable) {
+			if ( routePath.indexOf(routeInfo.path) >= 0) {
+				routeInfo.page.render(); // abstract를 통한 render() 의 사용
+				break;
+			}
+		}
+	}
+
 }
 
-function newsFeed(): void {
-	const api = new NewsFeedApi();
-	let newsFeed: NewsFeed[] = store.feeds;
-	const newsList = [];
-	const maxPage = newsFeed.length % 10 === 0 ? newsFeed.length / 10 : newsFeed.length / 10 + 1;
+class NewsFeedView extends View {
+	private api: NewsFeedApi;
+	private feeds: NewsFeed[];
 
-	let template = `
+	constructor(containerId:string) {
+		let template = `
 	<div class="bg-gray-600 min-h-screen">
 	<div class="bg-white text-xl">
 		<div class="mx-auto px-4">
@@ -121,48 +194,64 @@ function newsFeed(): void {
 	</div>
 </div>
 	`
+		super(containerId, template);
 
-	if(newsFeed.length === 0) {
-		newsFeed = store.feeds = makeFeeds(api.getData());
+		this.api = new NewsFeedApi();
+		this.feeds = store.feeds;
+		
+		
+		if (this.feeds.length === 0) {
+			this.feeds = store.feeds = this.api.getData();
+			this.makeFeeds();
+		}
+		// console.log(newsFeed)
+		
+		
 	}
-	// console.log(newsFeed)
-
-	for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; ++i) {
-    newsList.push(`
-      <div class="p-6 ${newsFeed[i].read ? 'bg-red-200' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+	
+	render(): void {
+		store.currentPage = Number(location.hash.substr(7) || 1);
+		const maxPage = this.feeds.length % 10 === 0 ? this.feeds.length / 10 : this.feeds.length / 10 + 1;
+		for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; ++i) {
+			const {id, title, comments_count, user, points, time_ago, read} = this.feeds[i];
+			this.addHtml(`
+      <div class="p-6 ${read ? 'bg-red-200' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
           <div class="flex-auto">
-            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+            <a href="#/show/${id}">${title}</a>  
           </div>
           <div class="text-center text-sm">
-            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${comments_count}</div>
           </div>
         </div>
         <div class="flex mt-3">
           <div class="grid grid-cols-3 text-sm text-gray-500">
-            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
-            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+            <div><i class="fas fa-user mr-1"></i>${user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${points}</div>
+            <div><i class="far fa-clock mr-1"></i>${time_ago}</div>
           </div>  
         </div>
       </div>    
     `);
-	};
+		};
 
-	template = template.replace('{{__news_feed__}}', newsList.join(''))
-	template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1))
-	template = template.replace('{{__next_page__}}', String(store.currentPage !== maxPage ? store.currentPage + 1 : maxPage))
-	
-	updateView(template);
+		this.setTemplateData('news_feed', this.getHtml());
+		this.setTemplateData('prev_page', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+		this.setTemplateData('next_page', String(store.currentPage !== maxPage ? store.currentPage + 1 : maxPage));
+
+		this.updateView();
+	}
+
+	private makeFeeds(): void {
+		for (let i = 0; i < this.feeds.length; ++i) {
+			this.feeds[i].read = false;
+		}
+	}
 }
 
-function newsDetail(): void {
-	// console.log('해시가 변경되었습니다.')
-	// console.log(this.location) url 주소에 대한 정보로 hash에 대한 값을 추출 가능.
-	const id = location.hash.substr(7) // #으로 시작하는 해쉬값에서 #을 제거하여 id 값만을 순수하게 추출하여 저장
-	const api = new NewsDetailApi();
-	const newsContent = api.getData(id);
-  let template = `
+class NewsDetailView extends View {
+	constructor(containerId: string) {
+		let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
         <div class="mx-auto px-4">
@@ -171,7 +260,7 @@ function newsDetail(): void {
               <h1 class="font-extrabold">Hacker News</h1>
             </div>
             <div class="items-center justify-end">
-              <a href="#/page/${store.currentPage}" class="text-gray-500">
+              <a href="#/page/{{__currentPage__}}" class="text-gray-500">
                 <i class="fa fa-times"></i>
               </a>
             </div>
@@ -180,9 +269,9 @@ function newsDetail(): void {
       </div>
 
       <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-        <h2>${newsContent.title}</h2>
+        <h2>{{__title__}}</h2>
         <div class="text-gray-400 h-20">
-          ${newsContent.content}
+          {{__content__}}
         </div>
 
         {{__comments__}}
@@ -191,59 +280,63 @@ function newsDetail(): void {
     </div>
   `;
 
-	for (let i =0 ; i < store.feeds.length; ++i) {
-		if(store.feeds[i].id === +id) {
-			store.feeds[i].read = true;
-			break;
+	super(containerId, template)
+	}
+
+	render() {
+		// console.log('해시가 변경되었습니다.')
+		// console.log(this.location) url 주소에 대한 정보로 hash에 대한 값을 추출 가능.
+		const id = location.hash.substr(7) // #으로 시작하는 해쉬값에서 #을 제거하여 id 값만을 순수하게 추출하여 저장
+		const api = new NewsDetailApi();
+		const newsDetail: NewsDetail = api.getData(id);
+
+		for (let i = 0; i < store.feeds.length; ++i) {
+			if (store.feeds[i].id === +id) {
+				store.feeds[i].read = true;
+				break;
+			}
 		}
+
+		this.setTemplateData('comments', this.makeComment(newsDetail.comments));
+		this.setTemplateData('currentPage', String(store.currentPage));
+		this.setTemplateData('title', newsDetail.title)
+		this.setTemplateData('content', newsDetail.content);
+
+		this.updateView();
 	}
 
-	updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)))
-}
-
-function makeComment(comments:NewsComment[]): string {
-	const commentString = [];
-
-	for(let i=0; i < comments.length; ++i) {
-		const comment: NewsComment = comments[i];
-
-		commentString.push(`
-			<div style="padding-left: ${comment.level * 40}px;" class="mt-4">
-				<div class="text-gray-400">
-					<i class="fa fa-sort-up mr-2"></i>
-					<strong>${comment.user}</strong> ${comment.time_ago}
-				</div>
-				<p class="text-gray-700">${comment.content}</p>
-			</div>      
-		`);
-
-		if(comment.comments.length > 0) {
-			commentString.push(makeComment(comment.comments));
+	makeComment(comments: NewsComment[]): string {
+		for (let i = 0; i < comments.length; ++i) {
+			const comment: NewsComment = comments[i];
+	
+			this.addHtml(`
+				<div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+					<div class="text-gray-400">
+						<i class="fa fa-sort-up mr-2"></i>
+						<strong>${comment.user}</strong> ${comment.time_ago}
+					</div>
+					<p class="text-gray-700">${comment.content}</p>
+				</div>      
+			`);
+	
+			if (comment.comments.length > 0) {
+				this.addHtml(this.makeComment(comment.comments));
+			}
 		}
-	}
-
-	// console.log(commentString);
-	return commentString.join('')
-}
-
-
-// 해쉬값에 따라 어떤 함수를 실행하여 사용자에게 제공할 것인지리를 판단하는 함수.
-// 화면 전환의 주요 쟁점
-function router(): void {
-	const routePath = location.hash;
-	// console.log(routePath); // #/page/(number)
-
-	if (routePath === '') {
-		newsFeed();
-	} else if (routePath.indexOf('#/page/') >= 0) {
-		store.currentPage = +routePath.substr(7);
-		newsFeed();
-	} else {
-		newsDetail();
+	
+		// console.log(commentString);
+		return this.getHtml();
 	}
 }
 
-window.addEventListener('hashchange', router);
+const router : Router = new Router();
+const newsFeedView = new NewsFeedView('root');
+const newsDetailView = new NewsDetailView('root');
 
-router();
+// 페이지 구성과 구현
+router.setDefaultPage(newsFeedView); //초기 페이지 설정으로 router에서 인자가 없을 때 구현할 초기 페이지
 
+router.addRoutePath('/page/', newsFeedView); // hash 값에 page가 있을 때엔 newsFeedView를 출력할 수 있도록 페이지 출력 설정
+router.addRoutePath('/show/', newsDetailView); // hash 값에 show가 있을 때엔 newsDetailView를 출력할 수 있도록 페이지 출력 설정
+
+router.route();
